4d90ab00bdae85b5a77c052a7ba5aebb279a5ad8
Ana Julya Borges <borgesanajulya@gmail.com>
2025-07-01 11:40:51 -0300

    Alterações

--- atividadeCalc.java
+++ atividadeCalc.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class atividadeCalc {
 
+    //Método menu
     public static String menu(Scanner scanner){
         System.out.println("---Calculadora---");
         System.out.println("Escolha um operador: +, -, *, / e %");
@@ -9,26 +10,31 @@ public class atividadeCalc {
         return operador;
     }
 
+    //Método soamar
     public static double somar(double numero1, double numero2){
         double soma =  numero1 + numero2;
         return soma;
     }
 
+    //Método subtrair
     public static double subtrair(double numero1, double numero2){
         double diminuir =  numero1 - numero2;
         return diminuir;
     }
 
+    //Método multiplicar
     public static double multiplicar(double numero1, double numero2){
         double multiplica =  numero1 * numero2;
         return multiplica;
     }
 
+    //Método dividir
     public static double dividir(double numero1, double numero2){
         double divide =  numero1 / numero2;
         return divide;
     }
 
+    //Método resto da divisão
     public static double restoDivisao(double numero1, double numero2){
         double resto =  numero1 % numero2;
         return resto;
@@ -37,12 +43,16 @@ public class atividadeCalc {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        //Chamando método e armazenando o resultado
         String operador = menu(scanner);
+        //Transformando em caractere
         char caractere = operador.charAt(0);
+
         System.out.println("Digite o primeiro numero: ");
         double numero1 = scanner.nextDouble();
         System.out.println("Digite o segundo numero: ");
         double numero2 = scanner.nextDouble();
+        
         double resultado = 0;
 
         switch (caractere){
